5691ece0f8b62e0cb5fd4291b5a8a1650d22022c
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2020-11-22 18:14:58 -0300

    Avanade

--- Configuração da arquitetura back-end com.NET Core/CA back-end com .NET Core/Filters/ValidacaoModelStateCustomizado.cs
+++ Configuração da arquitetura back-end com.NET Core/CA back-end com .NET Core/Filters/ValidacaoModelStateCustomizado.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CA_back_end_com_.NET_Core.Models.Usuarios;
+using CA_back_end_com_.NET_Core.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace CA_back_end_com_.NET_Core.Filters
 {
-    public class ValidacaoModelStateCustomizado
+    public class ValidacaoModelStateCustomizado : ActionFilterAttribute
     {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
+            }
+            return Ok(loginViewModelInPut);
+        }
     }
 }
